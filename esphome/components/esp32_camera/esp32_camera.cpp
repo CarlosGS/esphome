ecc8fd651800222bfcccd237ac25193131a75635
Carlos Garcia Saura <CarlosGS@users.noreply.github.com>
2021-11-08 22:18:44 +0100

    Update esp32_camera.cpp

--- esphome/components/esp32_camera/esp32_camera.cpp
+++ esphome/components/esp32_camera/esp32_camera.cpp
@@ -534,12 +534,10 @@ void ESP32Camera::loop() {
     return;
   }*/
   
-  sensor_t *s = esp_camera_sensor_get();
-  s->set_reg(s,0x43,0xff,0x40); // Magic to give us the frame faster
+  //sensor_t *s = esp_camera_sensor_get();
+  //s->set_reg(s,0x43,0xff,0x40); // Magic to give us the frame faster
   
-camera_fb_t * fb = NULL;
-
-  fb = esp_camera_fb_get();
+camera_fb_t * fb = esp_camera_fb_get();
 
 
   /*int retries=0;
