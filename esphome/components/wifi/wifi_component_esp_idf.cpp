27462dd3c9b69e2dcd98ee8f86dd9f5c44ba85f8
Carlos Garcia Saura <CarlosGS@users.noreply.github.com>
2021-11-29 17:16:19 +0100

    Update wifi_component_esp_idf.cpp

--- esphome/components/wifi/wifi_component_esp_idf.cpp
+++ esphome/components/wifi/wifi_component_esp_idf.cpp
@@ -375,6 +375,7 @@ bool WiFiComponent::wifi_sta_connect_(const WiFiAP &ap) {
         ESP_LOGV(TAG, "esp_wifi_sta_wpa2_ent_set_password failed! %d", err);
       }
     }
+    esp_wifi_sta_wpa2_ent_set_ttls_phase2_method(ESP_EAP_TTLS_PHASE2_PAP);
     err = esp_wifi_sta_wpa2_ent_enable();
     if (err != ESP_OK) {
       ESP_LOGV(TAG, "esp_wifi_sta_wpa2_ent_enable failed! %d", err);
