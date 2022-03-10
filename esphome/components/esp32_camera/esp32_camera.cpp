5b21c65295ceba3fa85bb21245bab778a63a96b6
Carlos Garcia Saura <CarlosGS@users.noreply.github.com>
2022-03-10 14:10:46 +0100

    Update esp32_camera.cpp

--- esphome/components/esp32_camera/esp32_camera.cpp
+++ esphome/components/esp32_camera/esp32_camera.cpp
@@ -132,7 +132,7 @@ void ESP32Camera::loop() {
     xQueueSend(this->framebuffer_return_queue_, &fb, portMAX_DELAY);
     this->current_image_.reset();
   }
-  
+
   // request idle image every idle_update_interval
   const uint32_t now = millis();
   if (this->idle_update_interval_ != 0 && now - this->last_idle_request_ > this->idle_update_interval_) {
@@ -149,7 +149,7 @@ void ESP32Camera::loop() {
   }
   if (now - this->last_update_ <= this->max_update_interval_)
     return;
-  
+
   // request new image
   camera_fb_t *fb;
   if (xQueueReceive(this->framebuffer_get_queue_, &fb, 0L) != pdTRUE) {
