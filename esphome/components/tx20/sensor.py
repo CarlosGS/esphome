39391f069def22a4fd0ccd20b7e599d53127619d
Carlos Garcia Saura <CarlosGS@users.noreply.github.com>
2022-05-08 17:33:28 +0200

    Update sensor.py

--- esphome/components/tx20/sensor.py
+++ esphome/components/tx20/sensor.py
@@ -17,6 +17,8 @@ from esphome.const import (
 tx20_ns = cg.esphome_ns.namespace("tx20")
 Tx20Component = tx20_ns.class_("Tx20Component", cg.Component)
 
+IGNORE_INVERTED = "ignore_inverted"
+
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(Tx20Component),
@@ -31,6 +33,7 @@ CONFIG_SCHEMA = cv.Schema(
             icon=ICON_SIGN_DIRECTION,
             accuracy_decimals=1,
         ),
+        cv.Optional(IGNORE_INVERTED, default=False): cv.boolean,
         cv.Required(CONF_PIN): cv.All(pins.internal_gpio_input_pin_schema),
     }
 ).extend(cv.COMPONENT_SCHEMA)
@@ -49,6 +52,9 @@ async def to_code(config):
         conf = config[CONF_WIND_DIRECTION_DEGREES]
         sens = await sensor.new_sensor(conf)
         cg.add(var.set_wind_direction_degrees_sensor(sens))
+
+    if IGNORE_INVERTED in config:
+        cg.add(var.set_ignore_inverted(config[IGNORE_INVERTED]))
 
     pin = await cg.gpio_pin_expression(config[CONF_PIN])
     cg.add(var.set_pin(pin))
