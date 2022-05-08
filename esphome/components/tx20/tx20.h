543ccbae7a3c88d2cbed21a6a47a4d2e266ea4b5
Carlos Garcia Saura <CarlosGS@users.noreply.github.com>
2022-05-08 17:30:37 +0200

    Update tx20.h

--- esphome/components/tx20/tx20.h
+++ esphome/components/tx20/tx20.h
@@ -46,7 +46,7 @@ class Tx20Component : public Component {
   sensor::Sensor *wind_speed_sensor_;
   sensor::Sensor *wind_direction_degrees_sensor_;
   Tx20ComponentStore store_;
-  bool ignore_inverted_{true};
+  bool ignore_inverted_{false};
 };
 
 }  // namespace tx20
