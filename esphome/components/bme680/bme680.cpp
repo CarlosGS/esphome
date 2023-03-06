4567aaf9e01e70c629a20ce4e4f4c43889fb1f4a
Carlos Garcia Saura <CarlosGS@users.noreply.github.com>
2023-03-06 01:18:24 +0100

    Update bme680.cpp

--- esphome/components/bme680/bme680.cpp
+++ esphome/components/bme680/bme680.cpp
@@ -308,7 +308,7 @@ void BME680Component::read_data_() {
   float humidity = this->calc_humidity_(raw_humidity);
   float gas_resistance = NAN;
   if (data[14] & 0x20) {
-    gas_resistance = this->calc_gas_resistance_high_(raw_gas, gas_range);
+    gas_resistance = this->calc_gas_resistance_low_(raw_gas, gas_range);
   }
 
   ESP_LOGD(TAG, "Got temperature=%.1f°C pressure=%.1fhPa humidity=%.1f%% gas_resistance=%.1fΩ", temperature, pressure,
