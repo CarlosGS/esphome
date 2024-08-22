283f8a9aa1dcc8974c11b127df8d93e7019a73d2
Carlos García-Saura <CarlosGS@users.noreply.github.com>
2024-08-22 18:27:37 +0200

    Update gps.cpp

--- esphome/components/gps/gps.cpp
+++ esphome/components/gps/gps.cpp
@@ -31,8 +31,37 @@ void GPS::update() {
 }
 
 void GPS::loop() {
-  while (this->available() && !this->has_time_) {
+  while (this->available()) {
     if (this->tiny_gps_.encode(this->read())) {
+      if (tiny_gps.time.isValid() && tiny_gps.date.isValid() && tiny_gps.date.isUpdated() && tiny_gps.date.year() > 2023) {
+        ESPTime val{};
+        val.year = tiny_gps.date.year();
+        val.month = tiny_gps.date.month();
+        val.day_of_month = tiny_gps.date.day();
+        // Set these to valid value for  recalc_timestamp_utc - it's not used for calculation
+        val.day_of_week = 1;
+        val.day_of_year = 1;
+      
+        val.hour = tiny_gps.time.hour();
+        val.minute = tiny_gps.time.minute();
+        val.second = tiny_gps.time.second();
+        val.recalc_timestamp_utc(false);
+        uint32_t epoch = val.timestamp;
+        struct timeval timev {
+          .tv_sec = static_cast<time_t>(epoch), .tv_usec = 0,
+        };
+        ESP_LOGVV(TAG, "Got epoch %" PRIu32, epoch);
+        struct timezone tz = {0, 0};
+        int ret = settimeofday(&timev, &tz);
+        if (ret == EINVAL) {
+          // Some ESP8266 frameworks abort when timezone parameter is not NULL
+          // while ESP32 expects it not to be NULL
+          ret = settimeofday(&timev, nullptr);
+        }
+        if (ret != 0) {
+          ESP_LOGW(TAG, "setimeofday() failed with code %d", ret);
+        }
+      }
       if (tiny_gps_.location.isUpdated()) {
         this->latitude_ = tiny_gps_.location.lat();
         this->longitude_ = tiny_gps_.location.lng();
